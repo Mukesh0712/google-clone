239cf958adbe422cda84044369f420a9190ff969
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-06-24 18:05:25 +0530

    home-header added

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import Home from './pages/Home.js';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Google Clone</h1>
-
-      {/* Home (the one with the search on) */}
-
-      {/* SearchPage (The results page) */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/search' />
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
