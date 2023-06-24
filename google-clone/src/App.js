9b02b1c448566658e5efe426221bf0098fac7a55
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2023-06-24 16:32:19 +0530

    initial commit

---
+++ google-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Google Clone</h1>
+
+      {/* Home (the one with the search on) */}
+
+      {/* SearchPage (The results page) */}
+    </div>
+  );
+}
+
+export default App;
